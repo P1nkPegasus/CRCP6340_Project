76815b918f63fe3ba4594856e2399edee58845a7
pinkpegasus <rukiyadykes@gmail.com>
2025-01-24 00:44:06 -0600

    updated contact info page; basics

--- public/js/contact.js
+++ public/js/contact.js
@@ -2,19 +2,44 @@ import { Input, Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input, Ripple });
 (function () {
-    "use strict";
-    document
-    .querySelector('#contactFormBtn')
-    .addEventListener('click', (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log('Thanks for clicking');
-        let name = document.querySelector('#name').value;
-        let email = document.querySelector('#email').value;
-        let message = document.querySelector('#message').value;
-        console.log('name' + name);
-        console.log('email' + email);
-        console.log('message' + message);
+  "use strict";
+  document
+    .querySelector("#contactFormBtn")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log("Thanks for clicking");
+      let firstName = document.querySelector("#firstnameInput").value;
+      let lastName = document.querySelector("#lastnameInput").value;
+      let email = document.querySelector("#emailInput").value;
+      let message = document.querySelector("#messageInput").value;
+      console.log("name" + firstName);
+      console.log("name" + lastName);
+      console.log("email" + email);
+      console.log("message" + message);
     });
 
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (() => {
+    "use strict";
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
 })();
