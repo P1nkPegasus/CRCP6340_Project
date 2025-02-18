075dbbd7833d2e96c9df282af080a6cec689ce89
pinkpegasus <rukiyadykes@gmail.com>
2025-02-18 06:16:22 -0600

    created head template for DRY

--- app.js
+++ app.js
@@ -1,47 +1,66 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import * as utils from './utils/utils.js';  
+import express from "express";
+import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
 dotenv.config();
 const app = express();
 const port = 3000;
-let data = ['Project 1', 'Project 2', 'Project 3'];
-app.set('view engine', 'ejs');
+let data = ["Project 1", "Project 2", "Project 3"];
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-})
-
-// app.get('/about', (req, res) => {
-//   res.render('about.ejs');
-// })
-
-app.get('/contact', (req, res) => {
-  res.render('contact.ejs');
-})
-
-app.get('/newProject', (req, res) => {
-  res.render('newProject.ejs');
-})
-
-app.get('/project', (req, res) => {
-  res.render('project.ejs');
-})
-
-app.get('/projects', (req, res) => {
-  res.render('projects.ejs', {projects: data});
-})
-
-app.post('/mail', async(req, res) => {
-  await utils 
-  .sendMessage(req.body.subject, req.body.text)
-  .then(() => {
-    res.send({result: 'Success'});
-  })
-  .catch((error) => {
-    res.send({result: 'Error sending message'});
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Rukiya D.'s NFT Portfolio",
+    description: "Rukiya D.s NFT Portfolio",
   });
+});
+
+// app.get("/about", (req, res) => {
+//   res.render("about", {
+//     title: "About Rukiya D.",
+//     description: "A page for the artist's biography.",
+//   });
+// });
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Contact Rukiya D.",
+    description: "Contact Rukiya D.",
+  });
+});
+
+app.get("/newProject", (req, res) => {
+  res.render("newProject", {
+    title: "New Project",
+    description: "A page to start a new project",
+  });
+});
+
+app.get("/project", (req, res) => {
+  res.render("project", {
+    title: "Project",
+    description: "A page for one project.",
+  });
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects", {
+    title: "Projects",
+    description: "A page for multiple projects.",
+    projects: data,
+  });
+});
+
+app.post("/mail", async (req, res) => {
+  await utils
+    .sendMessage(req.body.subject, req.body.text)
+    .then(() => {
+      res.send({ result: "Success" });
+    })
+    .catch((error) => {
+      res.send({ result: "Error sending message" });
+    });
 });
 
 app.listen(port, () => {
