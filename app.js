088ab8b1f81c58a1cd1a760fc4b4e83f440535c6
pinkpegasus <rukiyadykes@gmail.com>
2025-02-18 03:19:55 -0600

    updated html to ejs and updated all links

--- app.js
+++ app.js
@@ -5,12 +5,33 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
+// app.get('/about', (req, res) => {
+//   res.render('about.ejs');
 // })
+
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs');
+})
+
+app.get('/newProject', (req, res) => {
+  res.render('newProject.ejs');
+})
+
+app.get('/project', (req, res) => {
+  res.render('project.ejs');
+})
+
+app.get('/projects', (req, res) => {
+  res.render('projects.ejs');
+})
 
 app.post('/mail', async(req, res) => {
   await utils 
