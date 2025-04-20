b1b3219bd6ae4a9aa8451a13e0c04f92a5ccd545
pinkpegasus <rukiyadykes@gmail.com>
2025-04-20 16:47:16 -0500

    removed console logs and updated db script

--- utils/database.js
+++ utils/database.js
@@ -33,8 +33,6 @@ export async function connect() {
       },
     })
     .promise();
-    console.log(process.env); // Check what env vars are actually available
-    console.log(cString); // Verify the connection string is correct
 }
 
 export async function getAllProjects() {
