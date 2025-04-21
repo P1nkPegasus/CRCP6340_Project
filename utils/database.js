61a6f35c7a7107285493de16f576ddd8a36e71b8
pinkpegasus <rukiyadykes@gmail.com>
2025-04-21 01:37:57 -0500

    made updates to database code to use cString and updated database

--- utils/database.js
+++ utils/database.js
@@ -20,17 +20,18 @@ export async function connect() {
     "?" + process.env.MYSQL_SSL;
     pool = mysql
     .createPool({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      port: process.env.MYSQL_PORT,
-      ssl: {
-        ca: fs.readFileSync(
-          "/Users/rookmac/Documents/School/SMU/CRCP_6340_Creative_Coding_App_Dev/CRCP6340_Project/ca-certificate.crt"
-        ), 
-        rejectUnauthorized: true, // Enforces SSL certificate validation
-      },
+      cString
+      // host: process.env.MYSQL_HOST,
+      // user: process.env.MYSQL_USER,
+      // password: process.env.MYSQL_PASSWORD,
+      // database: process.env.MYSQL_DATABASE,
+      // port: process.env.MYSQL_PORT,
+      // ssl: {
+      //   ca: fs.readFileSync(
+      //     "/Users/rookmac/Documents/School/SMU/CRCP_6340_Creative_Coding_App_Dev/CRCP6340_Project/ca-certificate.crt"
+      //   ), 
+      //   rejectUnauthorized: true, // Enforces SSL certificate validation
+      // },
     })
     .promise();
 }
